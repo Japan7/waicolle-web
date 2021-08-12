8e3dcbb18bc3ae9797feb3839d0b9797f4f36eeb
NextFire <git@yuru.moe>
2021-08-12 22:26:31 +0200

    by bot we said

--- pages/api/collage/import.ts
+++ pages/api/collage/import.ts
@@ -13,6 +13,6 @@ export const WAICOLLAGE_DATA: { [key: number]: WCItem[] } = {};
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const body = JSON.parse(req.body);
-  WAICOLLAGE_DATA[body.guild] = body.data;
+  WAICOLLAGE_DATA[body.bot] = body.data;
   res.status(200).json({ url: 'https://waicolle.yuru.moe/collage/' + body.guild });
 }
