9b8aa936e4671aea27a690aa9a291bd9c7b3d721
NextFire <git@yuru.moe>
2021-08-15 19:53:01 +0200

    restore manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/logo192.png" />
         <meta name="theme-color" content="#956DA6" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Japan7/waicolle-web" />
         <meta property="og:image" content="https://waicolle.yuru.moe/logo512.png" />
