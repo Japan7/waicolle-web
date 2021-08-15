2d88ed3f7e2ab01c897241bca9057867252d9117
NextFire <git@yuru.moe>
2021-08-15 19:46:40 +0200

    let's just use the original cdn css

--- components/MarkdownPage.tsx
+++ components/MarkdownPage.tsx
@@ -1,8 +1,12 @@
-import styles from './MarkdownPage.module.scss';
+import Head from 'next/head';
 
 export default function MarkdownPage({ children }: { children: React.ReactNode }) {
   return (
-    <div className={styles.markdownBody}>
+    <div className="markdown-body">
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/hyrious/github-markdown-css/github-markdown.css" />
+      </Head>
+
       {children}
 
       <style jsx global>{`
@@ -13,6 +17,22 @@ export default function MarkdownPage({ children }: { children: React.ReactNode }
         @media (prefers-color-scheme: dark) {
           html {
             background-color: #0d1117;
+          }
+        }
+      `}</style>
+
+      <style jsx>{`
+        .markdown-body {
+          box-sizing: border-box;
+          min-width: 200px;
+          max-width: 980px;
+          margin: 0 auto;
+          padding: 45px;
+        }
+      
+        @media (max-width: 767px) {
+          .markdown-body {
+            padding: 15px;
           }
         }
       `}</style>
