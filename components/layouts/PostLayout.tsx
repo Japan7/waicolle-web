f329920ad241e39f63a8886c5a99ac2242c6f790
NextFire <git@yuru.moe>
2021-10-11 17:40:48 +0200

    [postlayout] update

--- components/layouts/PostLayout.tsx
+++ components/layouts/PostLayout.tsx
@@ -3,12 +3,13 @@ import Live2D from '../Live2D';
 
 export default function PostLayout({ children, home }: { children: React.ReactNode, home?: boolean }) {
   return (
-    <div className="max-w-4xl mx-auto px-4">
-      <header className="text-blue-500 my-4">
-        {!home && <Link href="/"><a>← Back to home</a></Link>}
-      </header>
+    <div className="prose dark:prose-dark mx-auto p-4">
+      {!home &&
+        <header className="my-4">
+          <Link href="/"><a>← Back to home</a></Link>
+        </header>}
 
-      <article className="prose dark:prose-dark max-w-none">
+      <article>
         {children}
       </article>
 
