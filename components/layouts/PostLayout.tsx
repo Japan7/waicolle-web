64319fa06d69f05a84b7f205a691bdbd245fa1a9
NextFire <git@yuru.moe>
2021-10-12 12:33:37 +0200

    [postlayout] text-justify

--- components/layouts/PostLayout.tsx
+++ components/layouts/PostLayout.tsx
@@ -3,7 +3,7 @@ import Live2D from '../Live2D';
 
 export default function PostLayout({ children, home }: { children: React.ReactNode, home?: boolean }) {
   return (
-    <div className="prose dark:prose-dark mx-auto p-4">
+    <div className="prose dark:prose-dark text-justify mx-auto p-4">
       {!home &&
         <header className="my-4">
           <Link href="/"><a>← Back to home</a></Link>
