dbb96a4613021982035b65cc4490e18f3d0b25b0
NextFire <git@yuru.moe>
2022-03-06 21:50:01 +0100

    tailwind v3

--- components/layouts/PostLayout.tsx
+++ components/layouts/PostLayout.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function PostLayout({ children, home }: { children: React.ReactNode, home?: boolean }) {
   return (
-    <div className="prose dark:prose-dark text-justify mx-auto p-4">
+    <div className="prose dark:prose-invert text-justify mx-auto p-4">
       {!home &&
         <header className="my-4">
           <Link href="/"><a>← Back to home</a></Link>
