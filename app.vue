8efcfc414094ec5ec8d60ae0e5d90d1e7c1b8383
NextFire <git@yuru.moe>
2023-07-30 13:33:23 +0200

    move static head data to nuxt.config

--- app.vue
+++ app.vue
@@ -1,23 +1,3 @@
-<script setup lang="ts">
-useHead({
-  title: "Waifu Collection",
-  meta: [
-    { name: "description", content: "WaiColle companion web app" },
-    { name: "theme-color", content: "#956DA6" },
-    { property: "og:type", content: "website" },
-    { property: "og:site_name", content: "Japan7/waicolle-web" },
-    {
-      property: "og:image",
-      content: "https://waicolle.japan7.bde.enseeiht.fr/logo512.png",
-    },
-  ],
-  link: [
-    { rel: "manifest", href: "/manifest.json" },
-    { rel: "apple-touch-icon", href: "/logo192.png" },
-  ],
-});
-</script>
-
 <template>
   <NuxtPage />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,25 @@ export default defineNuxtConfig({
       defaultClientId: "",
     },
   },
+  app: {
+    head: {
+      title: "Waifu Collection",
+      meta: [
+        { name: "description", content: "WaiColle companion web app" },
+        { name: "theme-color", content: "#956DA6" },
+        { property: "og:type", content: "website" },
+        { property: "og:site_name", content: "Japan7/waicolle-web" },
+        {
+          property: "og:image",
+          content: "https://waicolle.japan7.bde.enseeiht.fr/logo512.png",
+        },
+      ],
+      link: [
+        { rel: "manifest", href: "/manifest.json" },
+        { rel: "apple-touch-icon", href: "/logo192.png" },
+      ],
+    },
+  },
   apollo: {
     clients: {
       default: {
