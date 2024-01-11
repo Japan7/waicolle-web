2387b84cbf39eb0b419eadd5568c98318e8cfabf
NextFire <git@yuru.moe>
2024-01-11 14:46:17 -0500

    remove compression middleware

--- server/middleware/compression.ts
+++
@@ -1,7 +0,0 @@
-import compression from "compression";
-
-const compress = compression();
-
-export default defineEventHandler((event) => {
-  compress(event.node.req as any, event.node.res as any, () => {});
-});
